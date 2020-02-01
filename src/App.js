6ddacf5b6912672ff6f1f60a3e924508a229f315
Joshua Mitchell <jlm346@txstate.edu>
2020-02-01 14:13:56 -0600

    BASIC skeleton of SS calendar with html

--- src/App.js
+++ src/App.js
@@ -1,70 +1,11 @@
 import React from "react";
 import "./App.css";
+import SSCalendar from "./SSCalendar";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>
-                Su
-              </th>
-              <th>
-                Mo
-              </th>
-              <th>
-                Tu
-              </th>
-              <th>
-                We
-              </th>
-              <th>
-                Th
-              </th>
-              <th>
-                Fr
-              </th>
-              <th>
-                Sa
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-              <td>
-                <button>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <SSCalendar />
     </div>
   );
 }
---
+++ src/SSCalendar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./SSCalendar.css";
+
+function SSCalendar() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Su</th>
+            <th>Mo</th>
+            <th>Tu</th>
+            <th>We</th>
+            <th>Th</th>
+            <th>Fr</th>
+            <th>Sa</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+            <td>
+              <button></button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SSCalendar;
