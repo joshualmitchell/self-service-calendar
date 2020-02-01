a406559d9187d9da1382fd82bccad6fa489c07fa
Joshua Mitchell <jlm346@txstate.edu>
2020-02-01 14:10:05 -0600

    BASIC Skeleton for calendar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>hi</p>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Su
+              </th>
+              <th>
+                Mo
+              </th>
+              <th>
+                Tu
+              </th>
+              <th>
+                We
+              </th>
+              <th>
+                Th
+              </th>
+              <th>
+                Fr
+              </th>
+              <th>
+                Sa
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+              <td>
+                <button>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
